5c5fa0a3809e4bb28a29b16fa4509cd8bc2a8eee
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-01 14:24:21 +0530

    testig successful.

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 import express from "express";
 import { createServer } from "http";
+import {Server} from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
 
+
+// setup _dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// to create app
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+
+
+// serve static files now
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// handle socket events
+io.on("connection", (socket)=>{
+    console.log("new user connected : ", socket.id);
+
+    socket.on("sendMessage", (msg)=>{
+        console.log("message from client : ", msg);
+        io.emit("messageFromServer", ` ${msg}`);
+    });
+
+    socket.on("disconnect", ()=>{
+        console.log("user disconnected : ", socket.id);
+    })
+});
+
+
+
 
 app.get("/", (req, res)=>{
     res.send("Hello, World!");
